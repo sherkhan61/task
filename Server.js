ffc794af025744f7a4f055debdd33de9112b4683
Sherkhan Tulkibay <sherkhan-92@bk.ru>
2022-07-01 00:00:34 +0600

    prepare deploy

--- Server.js
+++ Server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
@@ -8,6 +9,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(routes);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, './frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(
+        path.resolve(__dirname, './', 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
