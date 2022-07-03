bbb9e15189950bf9b31a2ef78ca0dd130911d0f3
Sherkhan Tulkibay <sherkhan-92@bk.ru>
2022-07-04 00:42:54 +0600

    end task

--- Server.js
+++ Server.js
@@ -2,8 +2,8 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const cors = require('cors');
 const routes = require('./routes/routes');
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
@@ -11,13 +11,15 @@ app.use(cors());
 app.use(routes);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, './frontend/build')))
+    app.use('/', express.static(path.join(__dirname, 'frontend', 'build')))
 
     app.get('*', (req, res) => res.sendFile(
-        path.resolve(__dirname, './', 'frontend', 'build', 'index.html')))
+        path.resolve(__dirname, 'frontend', 'build', 'index.html')))
 } else {
     app.get('/', (req, res) => res.send('Please set to production'))
 }
+
+const PORT = process.env.PORT || 5000
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -26,6 +28,7 @@ mongoose.connect(process.env.MONGO_URL, {
     .then(() => console.log('Mongodb connected...'))
     .catch((err) => console.error(err));
 
-app.listen(5000, () => {
-    console.log('App listening on port 5000!');
+app.listen(PORT, () => {
+    console.log('App listening on port', PORT);
 })
+
